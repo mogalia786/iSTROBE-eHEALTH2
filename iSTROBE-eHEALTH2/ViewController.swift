ddc92b9ba2d421c57d323bc22da6b76765ef8030
ebrahim <imac@heros-imac.local>
2016-09-27 11:04:58 +0200

    Initial Commit

---
+++ iSTROBE-eHEALTH2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSTROBE-eHEALTH2
+//
+//  Created by ebrahim on 9/27/16.
+//  Copyright © 2016 iSTROBE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
